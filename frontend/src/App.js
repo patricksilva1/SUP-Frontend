725e86cb72c2d75e4f5015fdc5447d43ea431d91
patricksilva1 <patricksilva5160@gmail.com>
2023-07-15 00:24:40 -0300

    feat: Added Tables (#1)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import axios from 'axios';
+// import { format } from 'date-fns';
+import moment from 'moment';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      startDate: '',
-      endDate: '',
+      startDate: null, // null em vez de uma string vazia
+      endDate: null, // null em vez de uma string vazia
       operatorName: '',
+      transferencias: [], // Adicionamos um novo estado para armazenar as transferências
     };
   }
 
+  handleStartDateChange = (date) => {
+    this.setState({
+      startDate: date,
+    });
+  };
+
+  handleEndDateChange = (date) => {
+    this.setState({
+      endDate: date,
+    });
+  };
+  
   handleInputChange = (event) => {
     const target = event.target;
     const name = target.name;
@@ -20,7 +35,6 @@ class App extends React.Component {
       [name]: value,
     });
   };
-
 
   handleSearch = () => {
     const { startDate, endDate } = this.state;
@@ -37,14 +51,16 @@ class App extends React.Component {
         // Manipular a resposta do backend aqui
         const transferencias = response.data; // Supondo que a resposta é um array de objetos Transferencia
         console.log(transferencias);
-        // Use os dados das transferências conforme necessário
+
+        // Atualizar o estado com as transferências recebidas
+        this.setState({ transferencias });
       })
       .catch((error) => {
         // Manipular os erros aqui
         console.error(error);
       });
   };
-  
+
   handleSearchByOperator = () => {
     const { operatorName } = this.state;
   
@@ -90,49 +106,181 @@ class App extends React.Component {
         console.error(error);
       });
   };
+  
+ // ...
 
-  render() {
-    const { startDate, endDate, operatorName } = this.state;
+render() {
+  const { startDate, endDate, operatorName, transferencias } = this.state;
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Banco Dashboard</h1>
-          <div>
-            <label htmlFor="startDate">Data de Início:</label>
-            <input
-              type="date"
-              id="startDate"
-              name="startDate"
-              value={startDate}
-              onChange={this.handleInputChange}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Patrick's Bank Dashboard</h1>
+        {/* ...outros elementos do cabeçalho... */}
+        <div>
+          <label htmlFor="startDate">Data de Início:</label>
+          <input
+            type="date"
+            id="startDate"
+            name="startDate"
+            value={startDate}
+            onChange={this.handleInputChange}
+            placeholder="dd/MM/yyyy" // Adicione o placeholder formatado desejado
+          />
+        </div>
+        <div>
+          <label htmlFor="endDate">Data de Fim:</label>
+          <input
+            type="date"
+            id="endDate"
+            name="endDate"
+            value={endDate}
+            onChange={this.handleInputChange}
+              placeholder="dd/MM/yyyy" // Adicione o placeholder formatado desejado
             />
-          </div>
-          <div>
-            <label htmlFor="endDate">Data de Fim:</label>
-            <input
-              type="date"
-              id="endDate"
-              name="endDate"
-              value={endDate}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="operatorName">Nome do Operador:</label>
-            <input
-              type="text"
-              id="operatorName"
-              name="operatorName"
-              value={operatorName}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <button onClick={this.handleSearch}>Pesquisar</button>
-        </header>
-      </div>
-    );
-  }
+        </div>
+        <div>
+          <label htmlFor="operatorName">Nome Operador Transacionado:</label>
+          <input
+            type="text"
+            id="operatorName"
+            name="operatorName"
+            value={operatorName}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <button onClick={this.handleSearch}>Pesquisar</button>
+
+{/* Renderização das tabelas */}
+<div className="table-container">
+  <div className="table-column">
+  <table className="table table-data-transfer">
+      <thead>
+        <tr>
+          <th>Transferencia ID</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transferencias.map((transferencia) => (
+          <tr key={transferencia.id}>
+            <td>{transferencia.id}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+
+  <div className="table-column">
+  <table className="table table-data-transfer">
+      <thead>
+        <tr>
+          <th>Data de Transferência</th>
+        </tr>
+      </thead>
+      <tbody>
+      {transferencias.map((transferencia) => (
+            <tr key={transferencia.id}>
+              <td>{moment(transferencia.dataTransferencia).format('DD/MM/YYYY')}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  </div>
+
+  <div className="table-column">
+  <table className="table table-data-transfer">
+      <thead>
+        <tr>
+          <th>Valor</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transferencias.map((transferencia) => (
+          <tr key={transferencia.id}>
+            <td>{transferencia.valor}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+
+  <div className="table-column">
+  <table className="table table-data-transfer">
+      <thead>
+        <tr>
+          <th>Tipo</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transferencias.map((transferencia) => (
+          <tr key={transferencia.id}>
+            <td>{transferencia.tipo}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+
+  <div className="table-column">
+  <table className="table table-data-transfer">
+      <thead>
+        <tr>
+          <th>Nome do Operador</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transferencias.map((transferencia) => (
+          <tr key={transferencia.id}>
+            <td>{transferencia.nomeOperadorTransacao}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+
+  <div className="table-column">
+  <table className="table table-data-transfer">
+      <thead>
+        <tr>
+          <th>Saldo Atual</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transferencias.map((transferencia) => (
+          <tr key={transferencia.id}>
+            <td>{transferencia.saldoAtual}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+
+  <div className="table-column">
+  <table className="table table-data-transfer">
+      <thead>
+        <tr>
+          <th>Conta Destino</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transferencias.map((transferencia) => (
+          <tr key={transferencia.id}>
+            <td>{transferencia.contaDestino}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+</div>
+
+
+      </header>
+    </div>
+  );
+}
+
+// ...
+
 }
 
 export default App;
